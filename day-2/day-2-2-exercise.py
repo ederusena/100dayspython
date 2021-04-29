041b25f2777aeb1f5ca63f8ff7b8438653047d7b
ederusena <eder.sena@live.com>
2021-04-29 08:39:19 -0300

    chore: Day 2

---
+++ day-2/day-2-2-exercise.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = int(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆1.8
+
+#Write your code below this line 👇
+BMI = weight // (height ** 2)
+print(int(BMI))
+
---
+++ day-2/main.py
@@ -0,0 +1,10 @@
+# DAta types
+
+# String types
+str = "Hello"
+arr = ""
+
+for c in reversed(range(len(str))):
+   arr += str[c]
+
+print(arr)
